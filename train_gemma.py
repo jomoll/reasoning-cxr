98b87842bf39bba7d5014fbeb85c5581c176838f
johannes-moll <johannes.moll@tum.de>
2025-06-18 08:48:31 +0000

    .

--- train_gemma.py
+++ train_gemma.py
@@ -12,28 +12,9 @@ model_id = "google/gemma-3-4b-pt" # or `google/gemma-3-12b-pt`, `google/gemma-3-
 system_message = "You are an expert radiologist."
 user_prompt = "You are given a chest X-ray image. Please assess different findings on the following scale: 0: none, 1: mild, 2: moderate, 3: severe, 4: very severe. The findings are: Heart Size, Pulmonary Congestion, Pleural Effusion Right, Pleural Effusion Left, Pulmonary Opacities Right, Pulmonary Opacities Left, Atelectasis Right, Atelectasis Left. Please use the following format for your response: " \
 "Heart Size: <value>, Pulmonary Congestion: <value>, Pleural Effusion Right: <value>, Pleural Effusion Left: <value>, Pulmonary Opacities Right: <value>, Pulmonary Opacities Left: <value>, Atelectasis Right: <value>, Atelectasis Left: <value>."
-# Check if GPU benefits from bfloat16
-if torch.cuda.get_device_capability()[0] < 8:
-    raise ValueError("GPU does not support bfloat16, please use a GPU that supports bfloat16.")
-
-# Define model init arguments
-model_kwargs = dict(
-    attn_implementation="eager", # Use "flash_attention_2" when running on Ampere or newer GPU
-    torch_dtype=torch.bfloat16, # What torch dtype to use, defaults to auto
-    device_map="auto", # Let torch decide how to load the model
-)
-
-# BitsAndBytesConfig int-4 config
-model_kwargs["quantization_config"] = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=model_kwargs["torch_dtype"],
-    bnb_4bit_quant_storage=model_kwargs["torch_dtype"],
-)
 
 # Load model and tokenizer
-model = AutoModelForImageTextToText.from_pretrained(model_id, **model_kwargs)
+model = AutoModelForImageTextToText.from_pretrained(model_id)
 processor = AutoProcessor.from_pretrained("google/gemma-3-4b-it")
 print(f"Model {model_id} loaded successfully.")
 
